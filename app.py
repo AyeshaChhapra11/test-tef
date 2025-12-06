7830661b9c38da82d7d474dad5b6246c5a1ee51b
AyeshaChhapra11 <ayesha.chhapra@hackerox.com>
2025-12-06 14:02:08 +0530

    Add Streamlit app and remove main.py; update main.tf and requirements

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Cloud Run Demo")
+
+st.title("Hello from Cloud Run + Streamlit 👋")
+st.write("This app was deployed from GitHub Actions.")
--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
 }
 
 provider "google" {
-  project = "gen-lang-client-0056426034"  # your project ID
-  region  = "us-central1"                 # or whatever region you use
+  project = "gen-lang-client-0056426034"
+  region  = "us-central1"
 }
 
 resource "google_storage_bucket" "demo_bucket" {
-  name                     = "moviant-gemini-bucket-12345"  
+  name                     = "moviant-gemini-bucket-12345"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
